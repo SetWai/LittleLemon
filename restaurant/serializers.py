c5f2d508be2168097ec011eb6ff90886387b6619
Set Wai <sw.setwai@gmail.com>
2025-11-11 01:47:25 +0400

    Creating DRF

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-class UserSerializer(serializers.ModelSerializer):
+from .models import Booking, MenuItem
+
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'inventory']
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,50 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework import status
 from django.shortcuts import render
+from .permissions import IsManager
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields = ['price']
+    filterset_fields = ['price']
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+    
+class SingleMenuItemView(generics.RetrieveDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAuthenticated, IsManager | IsAdminUser]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions() 
+    
+# from rest_framework.decorators import api_view
+# from .models import MenuItem
+# from .serializers import MenuItemSerializer
+# from rest_framework import generics
+
+# # Create your views here. 
+# class MenuItemsView(generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+# class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
