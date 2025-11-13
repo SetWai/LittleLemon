e7e1d1232c22649d2d440aa48b18bdb512b7b756
Set Wai <sw.setwai@gmail.com>
2025-11-13 20:22:02 +0400

    adding token based authenticator

--- restaurant/views.py
+++ restaurant/views.py
@@ -9,8 +9,17 @@ from rest_framework import viewsets
 from rest_framework import status
 from django.shortcuts import render
 from .permissions import IsManager
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
